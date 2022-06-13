93ba41a856800851c6502df0a1bf14bf80b1b3ca
Miguel Méndez <is727312@iteso.mx>
2022-06-13 16:04:47 -0700

    Can retrieve posts from the database, currently only showing on the logcat.

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -7,12 +7,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
     private Button btnLogOut;
+    private Button btnCaptureImage;
+    private Button btnSubmit;
+    private EditText etDescription;
+    private ImageView ivPostImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogOut = findViewById(R.id.btnLogOut);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        etDescription = findViewById(R.id.etDescription);
+        ivPostImage = findViewById(R.id.ivPostImage);
 
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +48,27 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        //Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        //Specify the ObjectID
+         query.findInBackground(new FindCallback<Post>() {
+             @Override
+             public void done(List<Post> posts, ParseException e) {
+                 if(e != null){
+                     Log.e(TAG, "Issues with getting posts: ", e);
+                     return;
+                 }
+                  for(Post post: posts){
+                      Log.i(TAG,  "Post: " +  post.getDescription() + ", Username: " + post.getUser().getUsername());
+                  }
+             }
+         });
+
     }
 }
